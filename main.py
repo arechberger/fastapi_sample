729aa114f0d0cedf9431f9c1fe042298edfaf994
Albert Rechberger <albert.rechberger@gmail.com>
2019-12-27 19:27:49 +0100

    add samples that order matters

--- main.py
+++ main.py
@@ -24,3 +24,13 @@ async def read_item(item_id: int, q: str = None):
 @app.put("/items/{item_id}")
 async def put_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_current_user():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
